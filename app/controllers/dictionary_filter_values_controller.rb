6c30049306f42eea9c3e6c5cf5e4ff9a1ea4148f
Marcin Szmacinski <marcin@vraith.com>
2019-04-28 13:47:16 +0200

    dictionary filter values - fully operational CRUD

--- app/controllers/dictionary_filter_values_controller.rb
+++ app/controllers/dictionary_filter_values_controller.rb
@@ -6,24 +6,39 @@ class DictionaryFilterValuesController < ApplicationController
     @dictionary_filter_values = DictionaryFilterValue.where(category_filter: @category_filter)
   end
 
-  def show
+  def new
+    @dictionary_filter_value = DictionaryFilterValue.new(category_filter: @category_filter)
+    @url = category_filter_dictionary_filter_values_path
+  end
 
+  def show
   end
 
   def edit
-
+    @url = category_filter_dictionary_filter_value_path(@category_filter, @dictionary_filter_value)
   end
 
   def create
-
+    @dictionary_filter_value = DictionaryFilterValue.new(dictionary_params)
+    @dictionary_filter_value.category_filter = @category_filter
+    if @dictionary_filter_value.save
+      redirect_to category_filter_dictionary_filter_value_path(@category_filter, @dictionary_filter_value)
+    else
+      render :new
+    end
   end
 
   def update
-
+    if @dictionary_filter_value.update(dictionary_params)
+      redirect_to category_filter_dictionary_filter_value_path(@category_filter, @dictionary_filter_value)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @dictionary_filter_value.destroy
+    redirect_to category_filter_dictionary_filter_values_path(@category_filter), notice: 'Dictionary value was successfully destroyed.'
   end
 
   private
@@ -34,5 +49,9 @@ class DictionaryFilterValuesController < ApplicationController
 
   def set_dictionary_filter_value
     @dictionary_filter_value = DictionaryFilterValue.find(params[:id])
+  end
+
+  def dictionary_params
+    params.require(:dictionary_filter_value).permit(:name)
   end
 end
